e502e4931651929c7162911fbff2d958b079dea2
volkan <16579273+mevolkan@users.noreply.github.com>
2024-10-16 10:53:36 +0300

    Delete actioon on driver component

--- src/app/pages/apps/drivers/drivers.component.ts
+++ src/app/pages/apps/drivers/drivers.component.ts
@@ -6,8 +6,6 @@ import { IForm } from "../forms/interfaces/IForm";
 import { driverForm } from '../forms/driver-registration-form-config';
 import { environment } from 'environment';
 
-
-
 @Component({
   selector: 'app-drivers',
   templateUrl: './drivers.component.html'
@@ -29,36 +27,31 @@ export class DriversComponent {
   tableHeading: string = "Drivers";
   tableData: any[] = [];
 
+  constructor(public dialog: MatDialog, private http: HttpClient) {}
 
-  constructor(public dialog: MatDialog, private http: HttpClient) {
-  }
-
-  // Lifecycle event to execute the api calls
+  // Lifecycle event to execute the API calls
   ngOnInit(): void {
-    this.getDrivers()
-    // this.tableData = employees
+    this.getDrivers();
   }
 
   // Fetch drivers from the backend
   getDrivers() {
     this.http.get(environment.apiUrl.concat("drivers?page=0&size=20"))
       .subscribe((res: any) => {
-        this.tableData = res
-        console.log("Getting drivers data: {}", res)
-      })
+        this.tableData = res;
+        console.log("Getting drivers data: {}", res);
+      });
   }
 
   addStudent(request: any): any {
     this.http.post(environment.apiUrl.concat("drivers"), request)
       .subscribe((res: any) => {
-        var driver = res
-        console.log("Added driver: {}", res)
-        return driver;
-      })
+        console.log("Added driver: {}", res);
+      });
   }
 
   onViewItem(record: any) {
-    console.log("Viewing a driver")
+    console.log("Viewing a driver");
     this.dialog.open(SchoolViewComponent, {
       data: {
         action: 'View',
@@ -68,42 +61,59 @@ export class DriversComponent {
   }
 
   onAddItem(record: any) {
-    console.log("Adding a driver")
+    console.log("Adding a driver");
     this.dialog.open(SchoolViewComponent, {
       data: {
         action: 'View',
         driverData: record,
         formInput: driverForm
-      }, // Pass relevant data
+      },
     }).afterClosed().subscribe(result => {
-      if (result) { // Check if dialog closed with a value
+      if (result && result.action === 'Add') {
         console.log("Creation value from Student View:", result);
-        // Use the received value (result) here
-        if (result.action === 'Add') {
-          this.addStudent(result.data);
-        }
+        this.addStudent(result.data);
       }
     });
   }
 
   onUpdateItem(record: any) {
-    console.log("Updating a driver")
-    this.dialog.open(SchoolViewComponent), {
+    console.log("Updating a driver");
+    this.dialog.open(SchoolViewComponent, {
       data: {
         action: 'Update',
         formInput: driverForm
       }
-    };
+    });
   }
 
   onDeleteItem(record: any) {
-    console.log("Deleting a driver")
-    this.dialog.open(SchoolViewComponent, {
+    console.log("Deleting a driver");
+    const dialogRef = this.dialog.open(SchoolViewComponent, {
       data: {
         action: 'Delete',
-        formInput: driverForm
+        local_data: record
       },
     });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result?.event === 'Delete') {
+        this.deleteDriver(record.id).subscribe(
+          response => {
+            console.log('Driver deleted successfully', response);
+            // Remove the deleted driver from the local list
+            this.tableData = this.tableData.filter(driver => driver.id !== record.id);
+          },
+          error => {
+            console.error('Error deleting driver', error);
+            dialogRef.componentInstance.local_data.errorMessage = error.error?.message || 'An error occurred while deleting the vehicle.';
+          }
+        );
+      }
+    });
+  }
+
+  deleteDriver(id: string) {
+    return this.http.delete(environment.apiUrl.concat(`drivers/${id}`));
   }
 
   // Filter records
@@ -111,9 +121,8 @@ export class DriversComponent {
     console.log("Filtering records of drivers: ", record);
     this.http.get(environment.apiUrl.concat("drivers?name.contains=" + record))
       .subscribe((res: any) => {
-        this.tableData = res
-        console.log("Getting drivers data: {}", res)
-      })
+        this.tableData = res;
+        console.log("Getting drivers data: {}", res);
+      });
   }
 }
-
